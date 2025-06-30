1f2e2a6b6a166fbc02c83f84ac404cdb4c391ad1
bunyakorn12 <37292@bpk.ac.th>
2025-06-30 15:01:25 +0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,10 @@
+light = input("Enter a traffic light: ")
+
+if light == "red":
+    print("Stop")
+elif light == "yellow":
+    print("Ready to stop")    
+elif light == "green":
+    print("Go")
+else:
+    print("That is not traffic light color")        
