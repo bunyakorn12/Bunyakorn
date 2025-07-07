e5439707f4aef3f7e6c34ba74c30584934bb0c95
bunyakorn12 <37292@bpk.ac.th>
2025-07-07 14:44:10 +0700

    Add files via upload

---
+++ 1-100.py
@@ -0,0 +1,18 @@
+for i in range (1,100):
+    print (i)
+print("100 รอบ i=100")    
+for i in range (7,77,7):
+    print (i)
+print("9 รอบ i=70")    
+for i in range (20,1,-2):
+    print (i)
+print("8 รอบ i=2")    
+for i in range (2,18,3):
+    print (i)
+print("5 รอบ i=17")        
+for i in range (55,0,-11):
+    print (i)
+print("4 รอบ i=11")    
+for i in range (1,0):
+    print (i)
+print("0 1 ไม่มีค่า")
