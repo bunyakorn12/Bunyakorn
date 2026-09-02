7bb1d5d44fa883a8966ea6fac2c261a626509399
bunyakorn12 <37292@bpk.ac.th>
2026-09-02 19:52:19 +0700

    Add files via upload

---
+++ 5/ใบงานที่-5/connect.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "sql304.infinityfree.com";
+$username = "if0_42361346";
+$password = "Winwin26352";
+$dbname = "if0_42361346_php";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+
+// แสดงข้อความเฉพาะตอนเปิด connect.php โดยตรง
+if (basename($_SERVER['SCRIPT_FILENAME']) === 'connect.php') {
+    echo "เชื่อมต่อสำเร็จ นายบุณยกร เฉลิมพรวิทิต";
+}
+
+?>
